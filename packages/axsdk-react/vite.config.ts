968e1deb9b2f657f7f1517a281c382289a3314e5
lordkeios <lordkeios@lilysnc.com>
2026-03-23 17:32:55 +0900

    vite config fixed, react: 0.3.4

--- packages/axsdk-react/vite.config.ts
+++ packages/axsdk-react/vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from 'node:path'
 import { defineConfig, esmExternalRequirePlugin } from 'vite'
 import react from '@vitejs/plugin-react'
+import preserveUseClientDirective from 'rollup-plugin-preserve-use-client';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preserveUseClientDirective()],
   build: {
     lib: {
       entry: resolve(import.meta.dirname, "src/lib.ts"),
