3348ee6c0025a2414f46f6d10c9ba413cad1f197
lordkeios <lordkeios@lilysnc.com>
2026-04-17 14:31:56 +0900

    fix: export DeferredCallManager from lib.ts build entry point

--- packages/axsdk-core/src/lib.ts
+++ packages/axsdk-core/src/lib.ts
@@ -1,6 +1,9 @@
 export * from './axsdk';
 export * from './axtools';
 export * from './axhandler';
+export * as DeferredCallManager from './deferred';
+export type { DeferFn, DeferOptions } from './deferred';
+export type { DeferredCall } from './store';
 
 export function isPromise(value: unknown): boolean {
   return !!value && (typeof value === 'function' || typeof value === 'object') && typeof (value as any).then === 'function'
